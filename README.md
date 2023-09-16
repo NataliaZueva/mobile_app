667b0cd516132d8d92cef46de3880d178d147042
Natalia Zueva <natta_liya@vk.com>
2023-09-16 12:43:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mobile_app
+# mobile_app
+# mobile_app
---
+++ src/09.12.kt
@@ -0,0 +1,8 @@
+//fun main_1() {
+//    print("Hello")
+//}
+fun main() {
+    print("Введите имя: ")
+    val name = readLine()
+    println("Ваше имя: $name")
+}
